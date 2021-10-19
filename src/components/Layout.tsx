fd2bf08f88a930d99d8c6321f9573cac82514b60
Greq <greqorian@gmail.com>
2021-10-19 16:07:03 +0200

    fix(Leyout): :art: fixed layout width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,11 +16,11 @@ const Layout = () => {
 
                 <Box flex={"1 2 auto"} bg={themeColors.background.normal} />
 
-                <Box id="intro_frame" display={isMobile ? "none" : "auto"} w={{ base: "100%", md: "20%" }} minW={{ base: "100%", md: "md" }} h="full" bg={'white'} border={themeBorder.border} boxShadow="md" rounded="md">
+                <Box id="intro_frame" display={isMobile ? "none" : "auto"}  minW={{ base: "100%", md: "md" }} h="full" bg={'white'} border={themeBorder.border} boxShadow="md" rounded="md">
                     <Intro />
                 </Box>
 
-                <Box id="appointmentform_frame" w={{ base: "100%", md: "20%" }} minW={{ base: "100%", md: "sm" }} h="full" bg={'white'} borderRight={themeBorder.border} boxShadow="md" rounded="md">
+                <Box id="appointmentform_frame"  minW={{ base: "100%", md: "md" }} h="full" bg={'white'} borderRight={themeBorder.border} boxShadow="md" rounded="md">
                     <AppointmentForm />
                 </Box>
 
